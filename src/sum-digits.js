77b8919e9847d85fc96c790a76e1b5ab48d896ae
Nadezhda Klinskaya <npantelyeva@gmail.com>
2024-12-28 12:27:33 +0300

    feat: add solutions for what-season and sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let arr = n.toString().split('').map((el) => +el);
+  let i = 0;
+  while (arr.length > 1) {
+    arr = arr.reduce((acc, el) => acc + el).toString().split('').map((el) => +el);
+  }
+  return +arr.join('');
 }
 
 module.exports = {
